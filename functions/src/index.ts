ca2b224a2caca226e84399a8089ed52d58f87878
Nikhil Kothari <nik.kothari22@live.com>
2021-11-29 02:01:25 +0530

    Promises are returned from functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -23,29 +23,33 @@ exports.processMessage = functions.handler.firestore.document
 
 
         axios.post('https://slack.com/api/chat.postMessage', slackAPIData, { headers })
-            .then(function (response: any) {
+            .then((response: any) => {
                 functions.logger.info("Message successfully sent")
                 if (config.messageCollection) {
-                    admin.firestore().collection(config.messageCollection).doc(snap.id).update({
+                    return admin.firestore().collection(config.messageCollection).doc(snap.id).update({
                         updatedOn: admin.firestore.FieldValue.serverTimestamp(),
                         response: response.data,
                         status: 'success'
                     });
+                } else {
+                    return
                 }
 
             })
-            .catch(function (error: any) {
+            .catch((error: any) => {
                 if (error.response) {
                     // The request was made and the server responded with a status code
                     // that falls out of the range of 2xx
 
                     functions.logger.error(error.response.data);
                     if (config.messageCollection) {
-                        admin.firestore().collection(config.messageCollection).doc(snap.id).update({
+                        return admin.firestore().collection(config.messageCollection).doc(snap.id).update({
                             updatedOn: admin.firestore.FieldValue.serverTimestamp(),
                             response: error.response.data,
                             status: 'error'
                         });
+                    } else {
+                        return
                     }
                 } else if (error.request) {
                     // The request was made but no response was received
@@ -53,21 +57,25 @@ exports.processMessage = functions.handler.firestore.document
                     // http.ClientRequest in node.js
                     functions.logger.error(error.request);
                     if (config.messageCollection) {
-                        admin.firestore().collection(config.messageCollection).doc(snap.id).update({
+                        return admin.firestore().collection(config.messageCollection).doc(snap.id).update({
                             updatedOn: admin.firestore.FieldValue.serverTimestamp(),
                             response: error.request,
                             status: 'error'
                         });
+                    } else {
+                        return
                     }
                 } else {
                     // Something happened in setting up the request that triggered an Error
                     functions.logger.error('Error', error.message);
                     if (config.messageCollection) {
-                        admin.firestore().collection(config.messageCollection).doc(snap.id).update({
+                        return admin.firestore().collection(config.messageCollection).doc(snap.id).update({
                             updatedOn: admin.firestore.FieldValue.serverTimestamp(),
                             response: error.message,
                             status: 'error'
                         });
+                    } else {
+                        return
                     }
                 }
             });
